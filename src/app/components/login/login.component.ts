f93b35076b433be0d47fd76328dd5ce21e8c6a4e
Jeff Austin <jeffaustin32@gmail.com>
2017-01-21 01:30:57 -0800

    Authenticate Service now communicates with API.  Further styled login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { Router }      from '@angular/router';
+import { Component, OnInit, trigger, state, style, transition, animate, keyframes } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('inputError', [
+      state('void', style({
+        'transform': 'translateX(65%) scale(0)'
+      })),
+      transition('void => *', animate('100ms ease-in')),
+      transition('* => void', animate('100ms ease-out'))
+    ]),
+     trigger('errorMessage', [
+      state('void', style({
+        'opacity': '0'
+      })),
+      state('*', style({
+        'opacity': '1'
+      })),
+      transition('void => *', animate('300ms ease-in')),
+      transition('* => void', animate('300ms ease-out'))
+    ])
+  ]
 })
 export class LoginComponent implements OnInit {
+  private credentials: any = { username: '', password: '' };
+  private loggingIn: boolean = false;
+  private errorMessage: string = "";
 
   constructor(public authService: AuthService, public router: Router) { }
 
@@ -16,15 +38,23 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    this.authService.login().subscribe(() => {
-      if (this.authService.isLoggedIn) {
-         // If no redirect has been set, use the default
+    this.errorMessage = "";
+    this.authService.login(this.credentials)
+      .subscribe(() => {
+        // Remove the logging in message
+        this.loggingIn = false;
+
+        // If no redirect has been set, use the default
         let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/dashboard';
 
         // Redirect the user
         this.router.navigate([redirect]);
-      }
-    });
+      }, (err) => {
+        console.log(err);
+        // Display the login error message
+        this.loggingIn = false;
+        this.errorMessage = err;
+      });
   }
 
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/timeout';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
@@ -11,15 +15,67 @@ export class AuthService {
 
   // Store the URL so we can redirect after logging in
   public redirectUrl: string;
+  private baseUrl: string = 'http://localhost:3000/api'
 
-  login(): Observable<boolean> {
-    return Observable.of(true).delay(1000).do(val => {
-      this.isLoggedIn = true;
+  constructor(private http: Http, private router: Router) { }
+
+  login(credentials) {
+    // Make a login request to the server
+    return Observable.create((subscriber) => {
+      this.http.post(this.baseUrl + '/login', {
+          username: credentials.username,
+          password: credentials.password
+        })
+        .map(res => res.json())
+        .subscribe(
+        (res) => {
+          // Store the token in local storage
+          localStorage.setItem('token', JSON.stringify(res.data.token));
+          this.isLoggedIn = true;
+
+          subscriber.next();
+          subscriber.complete();
+        },
+        (err) => {
+          console.log('here');
+          // Bad Request
+          if (err.status === 400) {
+            subscriber.error('Uh... You didn\'t enter all the required info.');
+          }
+          // Unauthorized
+          else if (err.status === 401) {
+          console.log('here 2');
+            subscriber.error('Whoops! You entered incorrect login information.'); 
+          } 
+          // Internal Server Error
+          else if (err.status === 500) {
+            subscriber.error('Yikes! Looks like there was a server error.')
+          }
+
+        });
     });
   }
 
-  logout(): void {
+  // Logout by removing the user's token from local storage
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
     this.isLoggedIn = false;
   }
 
+  // Check if the user data and token exist in storage
+  isAuthorized() {
+    if (localStorage.getItem('token')) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // Create an Authorization header to allow access to secured server endpoints
+  createAuthorizationHeader(): Headers {
+    let token: string = JSON.parse(localStorage.getItem('authData'));
+    let header = new Headers({ 'Authorization': token });
+    return header;
+  }
 }
